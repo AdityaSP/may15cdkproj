395f26578414818e13b4cdcde94a4787db8849c2
AdityaSP <sp.aditya@gmail.com>
2024-05-16 00:38:18 +0530

    Input parameters or Deployment parameters

--- lib/firstproj-stack.ts
+++ lib/firstproj-stack.ts
@@ -7,18 +7,25 @@ import { OrgS3Bucket } from "../orgconstructs/orgs3bucket";
 export class FirstprojStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    //cdk deploy --parameters expirationDaysParam=12
+    const epp = new cdk.CfnParameter(this, "expirationDaysParam", {
+      type: "Number",
+      default: 10,
+      minValue: 3,
+      maxValue: 180,
+    });
 
     //L1 construct
+    const r1: cdk.aws_s3.CfnBucket.RuleProperty = {
+      status: "Enabled",
+      expirationInDays: epp.valueAsNumber,
+    };
 
-    const r1 : cdk.aws_s3.CfnBucket.RuleProperty = {
-            status: "Enabled",
-            expirationInDays: 10,
-          }
-
-    const lcc : cdk.aws_s3.CfnBucket.LifecycleConfigurationProperty= {
+    const lcc: cdk.aws_s3.CfnBucket.LifecycleConfigurationProperty = {
       rules: [r1],
     };
-        
+
     const cfnbucketprops: CfnBucketProps = {
       lifecycleConfiguration: lcc,
     };
@@ -26,16 +33,17 @@ export class FirstprojStack extends cdk.Stack {
     new cdk.aws_s3.CfnBucket(this, "MyS31", cfnbucketprops);
 
     //L2 construct
-    const l2bucketprops :BucketProps = {
+    const l2bucketprops: BucketProps = {
       lifecycleRules: [
         {
-          expiration: cdk.Duration.days(3),
+          expiration: cdk.Duration.days(epp.valueAsNumber),
+          noncurrentVersionsToRetain: 4,
         },
       ],
     };
-    new cdk.aws_s3.Bucket(this, "MyS32", l2bucketprops)
+    new cdk.aws_s3.Bucket(this, "MyS32", l2bucketprops);
 
     //L3 construct
-    new OrgS3Bucket(this, "MyS3L3")
+    new OrgS3Bucket(this, "MyS3L3");
   }
 }
