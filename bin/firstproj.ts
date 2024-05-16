4f1f631b1196c32206cd305369d7b2a2bf558832
AdityaSP <sp.aditya@gmail.com>
2024-05-16 22:02:41 +0530

    Create EC2 Instance

--- bin/firstproj.ts
+++ bin/firstproj.ts
@@ -5,5 +5,11 @@ import { FirstprojStack } from '../lib/firstproj-stack';
 
 const app = new cdk.App();
 
+// console.log(process.env)
 
-new FirstprojStack(app, 'FirstprojStackAdi', {});
+new FirstprojStack(app, 'FirstprojStackAdi', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
--- lib/firstproj-stack.ts
+++ lib/firstproj-stack.ts
@@ -7,5 +7,23 @@ import { OrgS3Bucket } from "../orgconstructs/orgs3bucket";
 export class FirstprojStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //1. image ?
+    const linImage = new cdk.aws_ec2.AmazonLinuxImage();
+    //2. instance type ?
+    const insType = cdk.aws_ec2.InstanceType.of(cdk.aws_ec2.InstanceClass.T2, cdk.aws_ec2.InstanceSize.MICRO)
+
+    //3. vpc ?
+    const defaultVpc = cdk.aws_ec2.Vpc.fromLookup(this, 'default', {isDefault: true})
+
+    //4. key pair: forSession
+    const kp = cdk.aws_ec2.KeyPair.fromKeyPairName(this, 'forsessioncdkid', 'forSession')
+    //5. ec2
+    const myec2 = new cdk.aws_ec2.Instance(this, 'myInstance', {
+      machineImage: linImage,
+      instanceType: insType,
+      vpc: defaultVpc,
+      keyPair: kp
+    })
   }
 }
